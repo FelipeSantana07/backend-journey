9680701f2e82f2fb40c6f4f5c1252a7b7df712ff
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-07-09 17:00:57 -0300

    create trip/confirm trip

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,35 @@
-console.log("hello world!")
+import fastify from "fastify"
+import cors from '@fastify/cors'
+
+import { prisma } from "./lib/prisma"
+
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
+
+const app = fastify();
+
+app.register(cors,{
+   origin: '*'
+})
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip)
+app.register(confirmTrip)
+
+
+
+
+app.get('/teste', () => { return 'Hello World!' })
+app.get('/listar', async () => {
+   const trips = await prisma.trip.findMany()
+   return trips
+})
+
+app.listen({ port: 3333 }).then(() => {
+   console.log('Server running...')
+   console.log('http://localhost:3333')
+})
